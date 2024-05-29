27fe2adbe512338ae768739b2719f080e2b1fd44
Noahbreezy <104791143+Noahbreezy@users.noreply.github.com>
2024-05-29 17:56:06 +0200

    bugfix product undefined

--- src/order.js
+++ src/order.js
@@ -32,8 +32,8 @@ async function setupOrderConsumer(order, channel, msg) {
       try {
         const clientId = (await getClientIdByUuid(order.user_id)).facturatie;
         const client = await fossbilling.getClient('', clientId);
-        console.log(order)
-        const productId = await getClientIdByUuid(order.products[0].product.product_id);
+        console.log(order.products)
+        const productId = await getClientIdByUuid(order.products.product.product_id);
         order.products[0].product_id = productId;
         console.log("Order: " + JSON.stringify(order));
         const invoicePDFBase64 = await fossbilling.finishOrder(order, clientId);
