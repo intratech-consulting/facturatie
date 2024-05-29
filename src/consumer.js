462123124bea84120608c61a3a091cd0655db50d
ThomasKnoops <thomas@knoops.be>
2024-05-29 15:51:29 +0200

    fix: typeError

--- src/consumer.js
+++ src/consumer.js
@@ -1,6 +1,11 @@
 const { setupUserConsumer } = require("./user");
 const { setupOrderConsumer } = require("./order");
 const { setupProductConsumer } = require("./product");
+const constants = require("./constants");
+const Logger = require("./logger");
+const logger = Logger.getLogger();
+const { XMLParser } = require("fast-xml-parser");
+const parser = new XMLParser();
 
 async function setupConsumer(connection) {
   const channel = await connection.createChannel();
@@ -16,18 +21,21 @@ async function setupConsumer(connection) {
     queue,
     async function (msg) {
       logger.log(
-        "setupUserConsumer",
+        "setupConsumer",
         `Received message: ${msg.content.toString()}`,
         false,
       );
       const object = parser.parse(msg.content.toString());
       if (object.user) {
+        console.log("received user ", object.user)
         const user = object.user;
         await setupUserConsumer(user, channel, msg);
       } else if (object.order) {
+        console.log("received order ", object.order)
         const order = object.order;
         await setupOrderConsumer(order, channel, msg);
       } else if (object.product) {
+        console.log("received product ", object.product)
         const product = object.product;
         await setupProductConsumer(product, channel, msg);
       } else {
